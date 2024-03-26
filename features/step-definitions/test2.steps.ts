39ba58842ce1719b730588be0b5451116b7bb16e
Reggie Easton <reggie.easton5@gmail.com>
2024-03-26 13:59:26 -0400

    Add create property feature and step definitions

--- features/step-definitions/test2.steps.ts
+++ features/step-definitions/test2.steps.ts
@@ -1,19 +1,23 @@
-import { DataTable, Given } from '@cucumber/cucumber';
+import { DataTable, Given, Then } from '@cucumber/cucumber';
 import { Actor } from '@serenity-js/core';
 import { CreateCommunity } from '../../screenplay/tasks/create-community';
 import { Register } from '../../screenplay/tasks/register';
 import { CreateRole } from '../../screenplay/tasks/create-role';
+import { CreateProperty } from '../../screenplay/tasks/create-property';
+import { Ensure, equals } from '@serenity-js/assertions';
+import { PropertyInDb } from '../../screenplay/questions/property-in-db';
 
-Given('{actor} creates a community called {word}', function (actor: Actor, communityName: string) {
-  actor.attemptsTo(Register.asUser(), CreateCommunity.named(communityName));
+Given('{actor} creates a community called {word}', async function (actor: Actor, communityName: string) {
+  actor.attemptsTo(Register.asUser(), await CreateCommunity.named(communityName));
 });
 
-Given(
-  '{actor} creates the {word} role in {word} with the following permissions:',
-  async function (actor: Actor, roleName: string, communityName: string, dataTable: DataTable) {
-    actor.attemptsTo(CreateRole.inCommunity(communityName).asNewRoleNamed(roleName).withPermissions(dataTable.rowsHash()));
-  }
-);
+Then('{actor} creates a property called {word} in {word}', async function (actor: Actor, propertyName: string, communityName: string) {
+  // Write code here that turns the phrase above into concrete actions
+  await actor.attemptsTo(CreateProperty.inCommunity(communityName).asNewPropertyNamed(propertyName));
+});
 
-Given('{actor} is a member of {word}', function (actor: Actor, communityName: string) {
-  actor.attemptsTo(Register.asUser())})
+Then('the property {word} created by {actor} exists in the community {word}', async function (propertyName: string, actor: Actor, communityName: string) {
+  // Write code here that turns the phrase above into concrete actions
+
+  await actor.attemptsTo(Ensure.that((await PropertyInDb(propertyName)).community.name, equals(communityName)));
+});
--- features/test2.feature
+++ features/test2.feature
@@ -1,12 +1,6 @@
-Feature: Creating a community
+Feature: Creating a Reggie
 
-Scenario: Create a community
+Scenario: Create a property and community
   Given ReggieTheOwner creates a community called LegoWorld
-  Given ReggieTheOwner creates the LegoEmployee role in LegoWorld with the following permissions: 
-    | memberPermissions         | canEditOwnMemberProfile |
-    | communityPermissions      | canManageRolesAndPermissions, canManageSiteContent, canManageMembers |
-    | serviceTicketPermissions  | canManageTickets, CanAssignTickets |
-    | servicePermissions        | canManageServices|
-  Given BobTheWorker is a member of LegoWorld
-#  When ReggieTheOwner assigns the LegoEmployee role to BobTheWorker
-#  Then BobTheWorker should have LegoEmployee permissions
+  And ReggieTheOwner creates a property called LegoVillage in LegoWorld
+  Then the property LegoVillage created by ReggieTheOwner exist in the community LegoWorld
